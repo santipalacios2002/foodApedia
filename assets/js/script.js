66a7bdd1f12de3e4c4e23e507a8e7b4acb37c5a0
Savannah Kolb <savannahnkolb@gmail.com>
2021-05-06 21:23:32 -0400

    started next API AJAX function to pull Ingredients and Instruction'

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 // function that receives ingridients
 var recipeBulkTest // will delete later, for testing pusposes only
+var test
 function apiRecipes(iOne, iTwo, iThree) {
   $.ajax({
     url: `https://api.spoonacular.com/recipes/findByIngredients?apiKey=c163ad42a8f44434961017e44052c438&ingredients=${iOne},${iTwo},${iThree}&number=4&ranking=1`,
@@ -9,7 +10,7 @@ function apiRecipes(iOne, iTwo, iThree) {
       console.log('Ajax Reponse \n-------------');
       console.log(response);
       recipeBulkTest = response; // will delete later, for testing pusposes only
-      searchedRecipes(response);
+      test = searchedRecipes(response);
     })
     .catch(function (error) { // runs if an error happens
       console.log('error:', error);
@@ -20,15 +21,32 @@ function apiRecipes(iOne, iTwo, iThree) {
 function searchedRecipes(recipesBulk) {
   var recipes = [{
     'picture': recipesBulk[0].image,
-    'name': recipesBulk[0].title
+    'name': recipesBulk[0].title,
+    'recipeId': recipesBulk[0].id
   }]; //array for the images of the searched recipes 
   for (let index = 1; index < recipesBulk.length; index++) {
     recipes.push({
       'picture': recipesBulk[index].image,
-      'name': recipesBulk[index].title
+      'name': recipesBulk[index].title,
+      'recipeId': recipesBulk[index].id
     }); //array for the images of the searched recipes
     console.log(recipes)
-  }
+  } 
+  return recipes
+}
+
+function recipeInfo(iD) {
+  $.ajax({
+    url: `https://api.spoonacular.com/recipes/${iD}/information?apiKey=c163ad42a8f44434961017e44052c438`,
+    method: 'GET',
+  })
+    .then(function (response) { // runs if no error happens
+      console.log('Ajax Reponse \n-------------');
+      console.log(response);
+    })
+    .catch(function (error) { // runs if an error happens
+      console.log('error:', error);
+    });
 }
 
 
