275f7dc3926432571a30483dc0a11a071d91aebd
Fatimah2014 <80806004+Fatimah2014@users.noreply.github.com>
2021-05-08 10:45:41 -0400

    minor

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,12 +8,12 @@ $('#start').on('click', function () {
 //delelte this minor code pleeeaaassseeee//
   //history.back()
 
-  var redirectAboutus = './index.html'
+//   var redirectAboutus = './index.html'
 
-//event listener when "Lets begin" button is pressed and user gets redirected to the input page
+// //event listener when "Lets begin" button is pressed and user gets redirected to the input page
 
-$('#aboutus').on('click', function () {
-    document.location.replace(redirectAboutus)
-  })
+// $('#aboutus').on('click', function () {
+//     document.location.replace(redirectAboutus)
+//   })
 
   
