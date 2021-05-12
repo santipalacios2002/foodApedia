8bd3cfcc0b9fab60d2a18a8edea3c4fa5f6d6606
Savannah Kolb <savannahnkolb@gmail.com>
2021-05-11 22:26:09 -0400

    Back button in recipe.html navigates back to input page

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,7 @@ var redirectUrl = './input.html'
 //event listener when "Lets begin" button is pressed and user gets redirected to the input page
 
 $('#start').on('click', function () {
-    document.location.replace(redirectUrl)
-  })
+  document.location.replace(redirectUrl)
+})
 
 
-  
