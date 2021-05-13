0da36993a7b7b29009cde3e5e5e04e4bf40ecb50
Santiago Palacios <santipalacios2002@gmail.com>
2021-05-13 10:15:05 -0400

    working on modal for recipes

--- assets/js/input.js
+++ assets/js/input.js
@@ -1,30 +1,74 @@
 //fatimah added foundation tag
 $(document).foundation();
 
-
-var test //might delete
-
 //variable for reset button 5/8
 var resetButtonEl = $('#reset');
-
 // click handler for search button
 var mealIngredients = [];
+
+
+
+//When user adds an ingredient event listener for the "add to list" btn
+$('#clickme').on('click', function () {
+  //check how long the mealIngriedents is
+  // If mealIngredients is less than 5 add new food to the array
+  if (mealIngredients.length < 5) {
+    // check if the ingridientsInput is empty
+    if ($('#ingredientsInput').val() == "") {
+      // if ingridentsInput is empty then give user feedback alert .... this needs to be changed to modal ''''SAVANNAH
+      alert('please enter the ingredient')
+      return;
+    }
+    // append food item to item list
+    var ingredientInput = $('#ingredientsInput').val();
+    buildIngredientli(ingredientInput)
+    // add food item to mealIngredients gloabal array
+    mealIngredients.push($('#ingredientsInput').val())
+    //resets the placeholder for the ingredient input
+    $('#ingredientsInput').val('');
+  } else {
+    // If mealIngredients is greater than 5 give user feedback alert .... this needs to be changed to modal '''''SAVANNAH
+    alert('you can only enter 5 ingredients')
+    $('#ingredientsInput').val('');
+  }
+  console.log(mealIngredients)
+})
+
+//function that builds the ingredient element
+function buildIngredientli(ingredient) {
+  $('#gap').append(`<div class="callout" data-closable><button class="close-button" aria-label="Close alert" type="button" data-close><span class="item ${ingredient}" aria-hidden="true">&#10008;</span></button> <p>${ingredient}</p></div>`)
+  //if the 'x' is clicked, this function will get rid of the item in the main mealIngredients array
+  $('#gap').on('click', '.close-button', function (event) {
+    var deletedIngredient = event.target.classList[1];
+    //if deletedIngredient exists in the mealIngredients then take it out of the mealIngredients array
+    console.log(mealIngredients.indexOf(deletedIngredient))
+    if (mealIngredients.indexOf(deletedIngredient) > -1) {
+      mealIngredients.splice(mealIngredients.indexOf(deletedIngredient), 1);
+    }
+  });
+}
+
+
+
+
+
+//click event that will trigger when you click on "search for recipe ideas"
 $('#searchRecipeBtn').click(apiRecipes);
 
 // This function calls the Spoonacular API
 function apiRecipes() {
   console.log('worked!')
-  //use meal ingredients to build the URL for the Ajax call
+  //initialize string of ingredients to be used on API
   var ingredients = '';
   console.log(mealIngredients)
+  //use mealIngredients to build string to be used for ingredients for the Ajax call
   for (let index = 0; index < mealIngredients.length; index++) {
     ingredients = ingredients.concat(`${mealIngredients[index]},`);
-
   }
   //ajax calls the URL API and gets the info
   $.ajax({
     //use URL with mealIngredients from above
-    url: `https://api.spoonacular.com/recipes/findByIngredients?apiKey=7b25b6057a3f4e83b5f38c6173d65341&ingredients=${ingredients}&number=4&ranking=1`,
+    url: `https://api.spoonacular.com/recipes/findByIngredients?apiKey=5d31bfe82c014672b108b9646a510b3f&ingredients=${ingredients}&number=4&ranking=1`,
     method: 'GET',
   })
     //response = info gathered from API
@@ -39,7 +83,7 @@ function apiRecipes() {
     });
 }
 
-//pulls picture and title of recipes
+//pulls picture, title and ID of recipes
 function searchedRecipes(recipesBulk) {
   var recipes = [{
     'picture': recipesBulk[0].image,
@@ -54,19 +98,24 @@ function searchedRecipes(recipesBulk) {
     }); //array for the images of the searched recipes
     console.log(recipes)
   }
-  buildRecipesEl(recipes)
+  for (let index = 0; index < recipes.length; index++) {
+    recipeInfo(recipes[index].recipeId)
+  }
+
   return recipes
 }
 
 function recipeInfo(iD) {
   $.ajax({
-    url: `https://api.spoonacular.com/recipes/${iD}/information?apiKey=c163ad42a8f44434961017e44052c438`,
+    url: `https://api.spoonacular.com/recipes/${iD}/information?apiKey=5d31bfe82c014672b108b9646a510b3f`,
     method: 'GET',
   })
     .then(function (response) { // runs if no error happens
       console.log('Ajax Reponse from recipeInfo\n-------------');
       console.log(response);
-      buildChosenRecipeEl(response)
+      localStorage.setItem(`${iD}`, JSON.stringify(response));
+      buildRecipesModal(`${iD}`)
+      // buildChosenRecipeModal()
     })
     .catch(function (error) { // runs if an error happens
       console.log('error:', error);
@@ -80,96 +129,50 @@ $('#back').on('click', function () {
 })
 
 
-//event listener for the add to list btn
-$('#clickme').on('click', function () {
-  //check how long the mealIngriedents is
-  // If mealIngredients is less than 5 add new food to the array
-  if (mealIngredients.length < 5) {
-    // check if the ingridientsInput is empty
-    if ($('#ingredientsInput').val() == "") {
-      // if ingridentsInput is empty then give user feedback alert .... this needs to be changed to modal ''''SAVANNAH
-      alert('please enter the ingredient')
-      return;
-    }
-    // append food item to item list
-    var ingredientInput = $('#ingredientsInput').val();
-    buildIngredientli(ingredientInput)
-    // add food item to mealIngredients
-    mealIngredients.push($('#ingredientsInput').val())
-    $('#ingredientsInput').val('');
-  } else {
-    // If mealIngredients is greater than 5 give user feedback alert .... this needs to be changed to modal '''''SAVANNAH
-    alert('you can only enter 5 ingredients')
-    $('#ingredientsInput').val('');
-  }
-  console.log(mealIngredients)
-
-})
-
-//clear history
-function showClear() {
-  if (searchHistoryList.text() !== "") {
-    clearHistoryBtn.removeClass("hide");
-  }
-}
 
 
-//function that builds the ingredient list element
-function buildIngredientli(ingredient) {
-  $('#gap').append(`<div class="callout" data-closable><button class="close-button" aria-label="Close alert" type="button" data-close><span class="item ${ingredient}" aria-hidden="true">&#10008;</span></button> <p>${ingredient}</p></div>`)
-  //if the 'x' is clicked, this function will get rid of the item in the main mealIngredients array
-  $('#gap').on('click', '.close-button', function (event) {
-    var deletedIngredient = event.target.classList[1];
-    //if deletedIngredient exists in the mealIngredients then take it out of the mealIngredients array
-    console.log(mealIngredients.indexOf(deletedIngredient))
-    if (mealIngredients.indexOf(deletedIngredient) > -1) {
-      mealIngredients.splice(mealIngredients.indexOf(deletedIngredient), 1);
-    }
-  });
-}
 
-//function that builds the recipe elements  ..... this function needs to have the data-open
-function buildRecipesEl(suggestions) {
-  for (let index = 0; index < suggestions.length; index++) {
+//function that builds the recipe elements for the modal to be called ..... this function needs to have the data-open
+function buildRecipesModal(id) {
+  // for (let index = 0; index < suggestions.length; index++) {
     var containerEl = $('<div>');
     containerEl.addClass('recipe');
-    containerEl.attr('id', `result-${index + 1}`)
+    containerEl.attr('id', `resultid-${id}`)
     var headerEl = $('<h4>');
     headerEl.attr('style', 'font-family:Courgette, cursive; color:black')
     var imageEl = $('<img>');
     $('img').css('cursor', 'pointer');
-    var redirectUrl = './recipe.html'
 
     // imageEl.attr('style', 'border: 3px solid black; box-shadow: 10px 10px 10px black; display: grid; gap:30px')
-    imageEl.attr('src', suggestions[index].picture);
+    imageEl.attr('src', JSON.parse(localStorage.getItem(id)).image);
     imageEl.attr('alt', 'food image')
-    imageEl.attr('class', suggestions[index].recipeId)
-    imageEl.attr('data-open', 'result1') //added for modal
-    headerEl.text(suggestions[index].name)
+    // imageEl.attr('class', suggestions[index].recipeId)
+    imageEl.attr('data-open', `spoonacular-${id}`) //added for modal
+    headerEl.text(JSON.parse(localStorage.getItem(id)).title)
     containerEl.append(headerEl);
     containerEl.append(imageEl);
     $('#recipe-container').append(containerEl);
-    $('#recipe-container').children().eq(index).children('img').on('click', function (event) {  //click event for recipes images
-      //at the click of the event target, application will take you to the detailed recipe
-      //by extracting the recipe ID and using it in the next API call
-      recipeInfo(event.target.className)
-      localStorage.setItem("chosenMeal", JSON.stringify(event.target.className));
-      localStorage.setItem('responseForBackBtn', JSON.stringify(suggestions));
 
-      // document.location.assign(redirectUrl)
-      //took out because we do not want to navigate to new page, but rather modal
 
-    })
-  }
-  console.log("these are the suggestions", suggestions)
 
+  //   $('#recipe-container').children().eq(index).children('img').on('click', function (event) {  //click event for recipes images
+  //     //at the click of the event target, application will take you to the detailed recipe
+  //     //by extracting the recipe ID and using it in the next API call
+  //     recipeInfo(event.target.className)
+  //     localStorage.setItem("chosenMeal", JSON.stringify(event.target.className));
+  //     localStorage.setItem('responseForBackBtn', JSON.stringify(suggestions));
+
+  //     document.location.assign(redirectUrl)
+  //     took out because we do not want to navigate to new page, but rather modal
+
+  //   })
+  // }
+  console.log("these are the suggestions", id)
+
+// }
 }
-
-
-
-
 //funciton that builds the info of the actual chosen recipe .... this needs to be inside the modal
-function buildChosenRecipeEl(detailedRecipe) {
+function buildChosenRecipeModal(detailedRecipe) {
   var containerEl = $('<div>');
   containerEl.attr('class', 'instructions');
   var headerEl = $('<h4>');
@@ -199,7 +202,7 @@ function buildChosenRecipeEl(detailedRecipe) {
 
 function buildinstructions(id) {
   $.ajax({
-    url: `https://api.spoonacular.com/recipes/${id}/analyzedInstructions?apiKey=c163ad42a8f44434961017e44052c438`,
+    url: `https://api.spoonacular.com/recipes/${id}/analyzedInstructions?apiKey=5d31bfe82c014672b108b9646a510b3f`,
     method: 'GET',
   })
     .then(function (response) { // runs if no error happens
@@ -227,16 +230,24 @@ function buildinstructions(id) {
 }
 
 
+//clear history
+function showClear() {
+  if (searchHistoryList.text() !== "") {
+    clearHistoryBtn.removeClass("hide");
+  }
+}
+
+
+
+
+
+
+
+
+
 // function for reset button//
-resetButtonEl.on('click', function () { //'''''SAVANNAH
-  //load the responseForBackBtn information from the most recent search
-  //if responseForBackBtn doesn't exist then don't build the information from recent search
+resetButtonEl.on('click', function () { 
+  localStorage.clear();
   location.reload();
 });
-
-//funtion or straight js code that checks if there is some stored recipes  //'''''SAVANNAH
-  //if stored recipe exists in sotrage then call the buildRecipesEl
-  //if not, then do nothing
-
-
 
